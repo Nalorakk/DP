760344bfaba9ba85a2a6e627dd2656741b1e9e22
Alex <alisandr13@mail.ru>
2018-06-27 00:42:48 +0300

    two commit

---
+++ app/src/main/java/com/example/alex/diplom/info_groups.java
@@ -0,0 +1,13 @@
+package com.example.alex.diplom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class info_groups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_groups);
+    }
+}
